92cc4d552394d9767951a41092512b574bfd25ef
regien <gerardomalpartidavela@gmail.com>
2018-02-23 17:22:41 -0800

    first done - double check

--- 00-nemean_lion/nemean_lion.sh
+++ 00-nemean_lion/nemean_lion.sh
@@ -1,15 +1,17 @@
-
 # get packages
 apt-get install openssh-server
 
-NEW_PORT="9020"
+# declaring new port
+NEW_PORT="4300"
+
 # changing the values to the new port
 # Every port after 1024 is valid
-sed -i 's/^.*Port 22/Port $NEW_PORT/g' etc/ssh/sshd_config
+sed -i "s/^.*Port 22/Port $NEW_PORT/g" /etc/ssh/sshd_config
 
 # you can choose one or another, there are both the same so it doesnt matter
 service ssh restart
-#etc/init.d/ssh restart
+# etc/init.d/ssh restart
 
 
-#try connecting
+# try connecting using ssh
+# or checking connection with " netstat -tulpn "
