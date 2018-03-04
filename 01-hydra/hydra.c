4c1bd6776becfaa300ab0503a0f3b753ca14be7d
regien <gerardomalpartidavela@gmail.com>
2018-03-03 19:47:35 -0800

    hydra done, beautification in progress

--- 01-hydra/hydra.c
+++ 01-hydra/hydra.c
@@ -6,7 +6,7 @@
 /*   By: regien <gmalpart@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 16:30:19 by regien            #+#    #+#             */
-/*   Updated: 2018/03/02 16:35:13 by regien           ###   ########.fr       */
+/*   Updated: 2018/03/03 19:47:17 by gmalpart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,61 +31,62 @@ int main(int ac, char **av)
 	int	socketfd;
 	int	newsocketfd;
 	int	portnbr;
-	int	clilen;
+	//	int	clilen;
+	unsigned int	clilen;
 	int	n;
 	struct	sockaddr_in	serv_addr;
 	struct	sockaddr_in	cli_addr;
-
 	char	buff[250];
-	if (ac < 2)
-	{
-		printf("Invalid port, please select one from 1025 to 5000\n");
-		return (0);
-	}
+
+
+	if (ac != 2)
+		print_error("Invalid port, please select one from 1025 to 5000\n");
+	if (atoi(av[1]) > 1024 && atoi(av[1]) < 5001)
+		portnbr = atoi(av[1]);
+	else
+		print_error("invalid port number\n");
+
+
 
 	socketfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (socketfd < 0)
 		print_error("ERROR, creating the socket\n");
 	else
 		printf("Socket sucessfully created\n");
-	
-	
 	// keep writing
+
+	// initializing of serv_addr
 	bzero((char *) &serv_addr, sizeof(serv_addr));
-
-	portnbr = atoi(av[1]);
-
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(portnbr);
 
+
+
 	if (bind(socketfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-	{
 		print_error("ERROR on binding\n");
-	}
 	listen(socketfd, 5);
 	clilen = sizeof(cli_addr);
-	
-	newsocketfd = accept(socketfd, (struct sockaddr *) &cli_addr, &clilen);
 
+	newsocketfd = accept(socketfd, (struct sockaddr *) &cli_addr, &clilen);
 	if (newsocketfd < 0)
 		print_error("ERROR on accept\n");
+	else
+		printf("Connection ACCEPTED\n");
 
-	bzero(buff, 250);
-
-	n = read(newsocketfd, buff, 250);
-
-	if (n < 0)
-		print_error("ERROR reading from socket\n");
-	
-	printf("here's the message: %s\n", buff);
-	
-	n = write(newsocketfd, "i got your message", 18);
-	
-	if (n < 0)
-		print_error("ERROR writing from socket");
-
-//	asd
+//	while (1)
+//	{
+		bzero(buff, 250);
+		n = read(newsocketfd, buff, 250);
+		if (n < 0)
+			print_error("ERROR reading from socket\n");
+		if (!(strncmp(buff, "ping", 4)))
+		{
+			n = write(newsocketfd, "pong\npong\n", 11);
+			if (n < 0)
+				print_error("ERROR writing from socket");
+		}
+//	}
 
 	return (0);
 }
